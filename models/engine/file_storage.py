c3f2d17729e387ea62f58ac0a0d28b262c684450
Batman99SD <azozkrg1@gmail.com>
2024-05-27 04:27:47 -0700

    improved storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,25 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        if cls and id:
+            if cls in classes.values():
+                all_objs = self.all(cls)
+                for v in all_objs.values():
+                    if v.id == id:
+                        return v
+            return
+        return
+
+    def count(self, cls=None):
+        """count the number of objects in storage matching the given class"""
+        if not cls:
+            inst_all = self.all()
+            return len(inst_all)
+        if cls in classes.values():
+            insts_of_cls = self.all(cls)
+            return len(insts_of_cls)
+        if cls not in classes.values():
+            return
