62343127e4b56cafc223d8829480172186b06dee
wafa <wafaagaafar@gmail.com>
2024-05-27 04:52:38 -0700

    adding api

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Flask blueprint"""
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_engine(exeption):
+    """teardown"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
+
+if __name__ == "__main__":
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Flask app_view"""
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status')
+def api_status():
+    """status"""
+    response = {'status': "OK"}
+    return jsonify(response)
+
+"""
+Task 4 
+"""
+@app_views.route('/stats')
+def get_stats():
+    """
+
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Reviw'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+    }
+    return jsonify(stats)
