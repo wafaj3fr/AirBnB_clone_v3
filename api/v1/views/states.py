8acf37e04cd1a37ff3ee2538f20dc1de53640e78
wafa <wafaagaafar@gmail.com>
2024-05-28 08:32:28 -0700

    wafaa

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
+'''
 create state view
-"""
+'''
 
 from flask import jsonify, abort, request
 from models import storage
@@ -11,9 +11,9 @@ from api.v1.views import app_views
 
 @app_views.route('/states', strict_slashes=False)
 def get_states():
-    """
+    '''
     get all states
-    """
+    '''
     states = storage.all(State).values()
     states = [state.to_dict() for state in states]
     return jsonify(states)
@@ -21,9 +21,9 @@ def get_states():
 
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def get_state(state_id):
-    """
+    '''
     get a state
-    """
+    '''
     state = storage.get(State, state_id)
     if state is None:
         return abort(404)
@@ -34,9 +34,9 @@ def get_state(state_id):
     '/states/<state_id>', methods=['DELETE'], strict_slashes=False
 )
 def delete_state(state_id):
-    """
+    '''
     delete a state
-    """
+    '''
     state = storage.get(State, state_id)
     if state is None:
         return abort(404)
@@ -47,9 +47,9 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    """
+    '''
     create a state
-    """
+    '''
     if request.content_type != 'application/json':
         return abort(404, "Not a JSON")
     data = request.get_json()
@@ -65,9 +65,9 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """
+    '''
     update a state
-    """
+    '''
     state = storage.get(State, state_id)
     if state is None:
         return abort(404)
